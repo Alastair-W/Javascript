25b79249901c9a64fdc410cb0ddea060e8bbfa0b
RowdyOnline <rowdy14@gmail.com>
2020-11-12 13:23:24 -0800

    Alog Practice Loops

---
+++ Week_3_Session_1/loops.js
@@ -0,0 +1,18 @@
+// Print odds 1-20
+
+for (var num = 1; num <= 20; num++){
+    if (num % 2 == 0){
+
+    }
+    else {
+        console.log(num)
+    }
+}
+
+// Sum and Print 1-5
+var sum = 0;
+for (var newNum = 1; newNum <= 5; newNum++){
+    sum += newNum;
+    console.log(newNum)
+    console.log(sum)
+}
